e848e9e90c9530408c2febec57727410a10bbcec
Alexander F. Rødseth <rodseth@gmail.com>
2024-09-13 09:02:50 +0200

    Update the uz utility

--- cmd/uz/main.go
+++ cmd/uz/main.go
@@ -7,25 +7,29 @@ import (
 	"github.com/xyproto/unzip"
 )
 
+const versionString = "uz 1.0.0"
+
 func main() {
 	o := textoutput.NewTextOutput(true, true)
 	flag.Parse()
 	if len(flag.Args()) == 0 {
-		o.ErrExit("Provide a ZIP filename and (optionally) a directory to extract to.")
+		o.Printf("<blue>%s</blue>\n", versionString)
+		o.Println("Provide a ZIP filename and (optionally) a directory to extract to.")
+		return
 	}
 	zipfile := flag.Args()[0]
 	directory := "."
 	if len(flag.Args()) > 1 {
 		directory = flag.Args()[1]
-		o.Println(o.LightGreen("Extracting " + zipfile + " to " + directory + "..."))
+		o.Printf("<green>Extracting %s to %s...</green>\n", zipfile, directory)
 	} else {
-		o.Println(o.LightGreen("Extracting " + zipfile + "..."))
+		o.Printf("<green>Extracting %s...</green>\n", zipfile)
 	}
 	if err := unzip.FilterExtract(zipfile, directory, func(filename string) bool {
-		o.Println(o.LightBlue("Extracting " + filename))
-		return true
+		o.Printf("<blue>Extracting %s</blue>\n", filename)
+		return true // continue
 	}); err != nil {
 		o.ErrExit(err.Error())
 	}
-	o.Println(o.LightGreen("Done."))
+	o.Println("<green>Done.</green>")
 }
